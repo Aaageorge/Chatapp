0771fa3ab41df935c11d94e4adf2f26d5250ed77
Aaageorge <95921222+Aaageorge@users.noreply.github.com>
2026-06-24 12:05:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import cors from 'cors';
+
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
